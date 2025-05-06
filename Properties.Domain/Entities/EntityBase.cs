69319916dc4a34604850ad663ca67c91f89358b1
Bmez <daniel.bmez1@gmail.com>
2025-05-05 22:39:33 -0500

    Add entities

---
+++ Properties.Domain/Entities/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace Properties.Domain.Entities
+{
+    public abstract class EntityBase
+    {
+        public Guid Id { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+    }
+}
